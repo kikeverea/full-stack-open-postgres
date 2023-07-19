a220367304d4ac9d9b01f872f894b6e27b0b3366
Enrique Verea <kikeverea@gmail.com>
2023-07-19 12:35:52 +0200

    Add support for migrations rollback

--- util/db.js
+++ util/db.js
@@ -4,15 +4,17 @@ const { Umzug, SequelizeStorage } = require('umzug')
 
 const sequelize = new Sequelize(DATABASE_URL);
 
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js',
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+}
+
 const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: {
-      glob: 'migrations/*.js',
-    },
-    storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  })
+  const migrator = new Umzug(migrationConf)
 
   const migrations = await migrator.up()
   console.log('Migrations up to date', {
@@ -20,19 +22,32 @@ const runMigrations = async () => {
   })
 }
 
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+}
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
-    await runMigrations()
     console.log('Connected to database')
   }
   catch (e) {
     console.log('Failed to connect to the database')
-    console.log(e.message)
+    return process.exit(1)
+  }
+
+  try {
+    await runMigrations()
+  }
+  catch (e) {
+    console.log('Failed to run migration')
+    console.log('Caused by:', e.message)
     return process.exit(1)
   }
 
   return null
 }
 
-module.exports = { sequelize, connectToDatabase }
+module.exports = { sequelize, connectToDatabase, rollbackMigration }
